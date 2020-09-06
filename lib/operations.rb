32e30f7f4023bf2dc51013f9dfa9efb4a21445d0
Alie Stone <helloaliestone@gmail.com>
2020-09-06 13:27:24 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -4,11 +4,12 @@ def unsafe?(speed)
     return true
   elsif speed > 60
     return true
-  else false
+  else
+    return false 
 end
 
 def not_safe? (speed)
   speed > 60 ? true : false
   speed < 40 ? true : false
   end
-end 
+end
